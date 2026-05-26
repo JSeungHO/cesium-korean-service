f11fe89b5d11f4187a3b2e065fb5ac1fcb2f910e
JSeungHO <Juengsh0702@gmail.com>
2026-05-26 19:36:32 +0900

    지도 띄우기

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-// 나중에 브이월드 키를 여기에 넣을 예정입니다.
-const viewer = new Cesium.Viewer('cesiumContainer', {
-    terrainProvider: Cesium.createWorldTerrain()
-});
+import { CESIUM_ACCESS_TOKEN } from './config.js';
+
+Cesium.Ion.defaultAccessToken = CESIUM_ACCESS_TOKEN;
+
+async function initCesium() {
+    const terrainProvider = await Cesium.createWorldTerrainAsync();
+    
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+        terrainProvider: terrainProvider
+    });
+}
+
+initCesium();
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/cesium/Build/Cesium/*',
+          dest: 'cesium'
+        }
+      ]
+    })
+  ],
+  define: {
+    CESIUM_BASE_URL: JSON.stringify('/cesium/')
+  }
+});
